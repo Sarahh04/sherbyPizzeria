d78de4c279925fc889127043699c83fc4c64b80a
Maimouna Sarah Diakite <202092642@cegepsherbrooke.qc.ca>
2023-04-26 15:16:27 -0400

    Insertdela tableRole

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            // Vous pouvez ajouter d’autres "seeders" en les séparant par des virgules.
+            roleSeeder::class
+
+            ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
