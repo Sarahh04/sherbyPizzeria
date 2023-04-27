e7864dc58ad2b7878465f1d6fbd0b043927ed251
Amelie_Frechette <201559339@cegepsherbrooke.qc.ca>
2023-04-27 12:27:15 -0400

    nav terminer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Bienvenu {{ Auth::check() ? Auth::user()->name : "Utilisateur anonyme" }}
         </h2>
     </x-slot>
 
