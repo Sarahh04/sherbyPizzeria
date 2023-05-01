eba3b0454dd36694de4d4ba210c1e0e4a5ff8f7c
ThirdPartyHugo <hpskate26@gmail.com>
2023-05-01 10:37:19 -0400

    f

--- resources/views/gestionInventaire.blade.php
+++ resources/views/gestionInventaire.blade.php
@@ -1,8 +1,17 @@
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
 <header class="h-gestion-prod">
-    <h1>Gestion des menus</h1>
+    <h1>Gestion de l'inventaire</h1>
 </header>
+<article class="search-main">
+    <section class="search-container">
+        <form action="" class="search-form">
+            <input type="text" class="search-bar">
+            <input type="submit" value="Search inventory">
+        </form>
+    </section>
+</article>
 <article class="main-container-menu">
+
     <section class="product-menu-container">
         <table class="GeneratedTable">
             <thead>
@@ -112,7 +121,7 @@
     </section>
     <section class="product-form-container">
         <div class=" form-gestion">
-            <form action="">
+            <form action="" class="form-ajout-int">
                 <div>
                     <input type="text" placeholder="nom">
                 </div>
@@ -122,7 +131,7 @@
                 <div>
                     <input type="text" placeholder="utility">
                 </div>
-                <input type="submit" value="">
+                <input type="submit" value="Ajouter" class="add-int">
             </form>
         </div>
     </section>
