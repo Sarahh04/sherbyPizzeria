b8725431ec5433a574b88be78a6a8caa5c4f0f84
ThirdPartyHugo <hpskate26@gmail.com>
2023-05-02 15:06:51 -0400

    a

--- resources/views/gestionInventaire.blade.php
+++ resources/views/gestionInventaire.blade.php
@@ -1,142 +1,53 @@
 <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
-<header class="h-gestion-prod">
-    <h1>Gestion de l'inventaire</h1>
-</header>
-<article class="search-main">
-    <section class="search-container">
-        <form action="" class="search-form">
-            <input type="text" class="search-bar">
-            <input type="submit" value="Search inventory">
-        </form>
-    </section>
-</article>
-<article class="main-container-menu">
-
-    <section class="product-menu-container">
-        <table class="GeneratedTable">
-            <thead>
-                <tr>
-                    <th class="data-inv">Nom</th>
-                    <th class="data-inv">Qty</th>
-                    <th class="data-inv">Utilisation</th>
-                    <th class="data-inv">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-                <tr>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                    <td class="data-inv">Cell</td>
-                </tr>
-            </tbody>
-        </table>
-        <!-- Codes by Quackit.com -->
-
-
-    </section>
-    <section class="product-form-container">
-        <div class=" form-gestion">
-            <form action="" class="form-ajout-int">
-                <div>
-                    <input type="text" placeholder="nom">
-                </div>
-                <div>
-                    <input type="text" placeholder="qty">
-                </div>
-                <div>
-                    <input type="text" placeholder="utility">
-                </div>
-                <input type="submit" value="Ajouter" class="add-int">
+<x-app-layout>
+    <article class="search-main">
+        <section class="search-container">
+            <form action="" class="search-form">
+                <input type="text" class="search-bar">
+                <input type="submit" value="Search inventory">
             </form>
-        </div>
-    </section>
-</article>
+        </section>
+    </article>
+    <article class="main-container-menu">
 
+        <section class="product-menu-container">
+            <table class="GeneratedTable">
+                <thead>
+                    <tr>
+                        <th class="data-inv">Nom</th>
+                        <th class="data-inv">Qty</th>
+                        <th class="data-inv">Utilisation</th>
+                        <th class="data-inv">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="data-inv">Cell</td>
+                        <td class="data-inv">Cell</td>
+                        <td class="data-inv">Cell</td>
+                        <td class="data-inv">Cell</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+        <section class="product-form-container">
+            <div class=" form-gestion">
+                <form action="" class="form-ajout-int">
+                    <div>
+                        <input type="text" placeholder="nom">
+                    </div>
+                    <div>
+                        <input type="text" placeholder="qty">
+                    </div>
+                    <div>
+                        <input type="text" placeholder="utility">
+                    </div>
+                    <input type="submit" value="Ajouter" class="add-int">
+                </form>
+            </div>
+        </section>
+    </article>
+</x-app-layout>
 <style>
     table.GeneratedTable {
         width: 100%;
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,31 +6,33 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center pr-32">
                     <a href="{{ route('dashboard') }}">
-                        <img src="{{ asset('img/logoSherbyPizzeria.png') }}" alt="logoPizza" class="block h-9 w-auto fill-current">
+                        <img src="{{ asset('img/logoSherbyPizzeria.png') }}" alt="logoPizza"
+                            class="block h-9 w-auto fill-current">
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:flex">
                     <x-nav-link :href="route('gestionCommandes')" :active="request()->routeIs('dashboard')">
-                        <img src="{{ asset('img/commande.svg') }}" alt="nav_commande" class = "img_commande">
+                        <img src="{{ asset('img/commande.svg') }}" alt="nav_commande" class="img_commande">
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        <img src="{{ asset('img/menu.png') }}" alt="nav_menu" class = "img_menu">
+                    <x-nav-link :href="route('gestionProduits')" :active="request()->routeIs('dashboard')">
+                        <img src="{{ asset('img/menu.png') }}" alt="nav_menu" class="img_menu">
                     </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        <img src="{{ asset('img/inventaire.svg') }}" alt="nav_inventaire" class = "img_inventaire">
+                    <x-nav-link :href="route('gestionInventaire')" :active="request()->routeIs('dashboard')">
+                        <img src="{{ asset('img/inventaire.svg') }}" alt="nav_inventaire" class="img_inventaire">
                     </x-nav-link>
                     <?php
                     /* $user = Auth::check()
                     if ($user->roles()->where('role', 'like', 'Administrateur')->count() > 0){
-                     */?>
-                        <x-nav-link :href="route('indexUser')" :active="request()->routeIs('indexUser')">
-                            <img src="{{ asset('img/employes.svg') }}" alt="nav_employes" class = "img_employes">
-                        </x-nav-link>
-                        <x-nav-link :href="route('promotions')" :active="request()->routeIs('promotions')">
-                            <img src="{{ asset('img/promo.svg') }}" alt="nav_promo" class = "img_promo">
-                        </x-nav-link>
+                     */
+                    ?>
+                    <x-nav-link :href="route('indexUser')" :active="request()->routeIs('indexUser')">
+                        <img src="{{ asset('img/employes.svg') }}" alt="nav_employes" class="img_employes">
+                    </x-nav-link>
+                    <x-nav-link :href="route('promotions')" :active="request()->routeIs('promotions')">
+                        <img src="{{ asset('img/promo.svg') }}" alt="nav_promo" class="img_promo">
+                    </x-nav-link>
                     <?php
                     /* } */
                     ?>
@@ -41,12 +43,16 @@
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-12 py-6 text-sm leading-4 font-medium focus:outline-none transition ease-in-out duration-150 nav_btn_user">
-                            <div><img src="{{ asset('img/login.svg') }}" alt="nav_login" class = "img_login"></div>
+                        <button
+                            class="inline-flex items-center px-12 py-6 text-sm leading-4 font-medium focus:outline-none transition ease-in-out duration-150 nav_btn_user">
+                            <div><img src="{{ asset('img/login.svg') }}" alt="nav_login" class="img_login"></div>
 
                             <div class="ml-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                        clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
@@ -62,7 +68,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -73,10 +79,14 @@
 
             <!-- Hamburger -->
             <div class="-mr-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out nav_all_deroule">
+                <button @click="open = ! open"
+                    class="inline-flex items-center justify-center p-2 rounded-md focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out nav_all_deroule">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
