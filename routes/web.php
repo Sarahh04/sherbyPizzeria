5c857371dadb3be65de2ad4a60d529abdee082b5
Amelie_Frechette <201559339@cegepsherbrooke.qc.ca>
2023-05-06 12:25:23 -0400

    ajout middleware admin

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DelaiCeuilletteController;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\ClientController;
+use App\Http\Middleware\EnsureUserAdmin;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,57 +30,57 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::controller(TransactionController::class)->group(function () {
+        Route::get('/gestionCommandes', 'index')->name('gestionCommandes');
+        Route::get('/ajouterCommande', 'create')->name('ajouterCommande');
+        Route::get('/consulterCommande', 'index')->name('consulterCommande');
+        Route::get('/listerCommandes', 'index')->name('listerCommandes');
+        Route::get('/commande/{id}', 'show')->name('detailCommande');
+        Route::get('/resumeCommande', 'index')->name('resumeCommande');
+        Route::get('/extraitCommande/{id}', 'edit')->name('extraitCommande');
+        Route::get('/editerCommande/{id}', 'show')->name('editerCommande');
+        Route::get('/editerCommande/commande', 'update')->name('enregistrerCommande');
+        // Route::get('/supprimerCommande', 'destroy')->name('supprimerCommande');
+    });
+
+    Route::controller(ProduitController::class)->group(function () {
+        Route::get('/promotions', 'index')->name('promotions');
+        Route::get('/newPromotion', 'create')->name('newPromotion')->middleware(EnsureUserAdmin::class);
+        Route::get('/gestion/produits', 'store')->name('gestionProduits')->middleware(EnsureUserAdmin::class);
+        Route::get('/gestion/inventaire', 'index')->name('gestionInventaire')->middleware(EnsureUserAdmin::class);
+        Route::post('/gestion/inventaire/{nom}', 'update')->name('search');
+    });
+
+    Route::controller(DelaiCeuilletteController::class)->group(function () {
+        Route::get('/delais', 'index')->name('delais');
+        Route::get('/modifier/delais', 'edit')->name('modifierDelais');
+        Route::get('/definir/delais', 'create')->name('definirDelais');
+    });
+
+    Route::controller(ClientController::class)->group(function () {
+        Route::get('/clients', 'index')->name('consulterClient');
+        Route::get('/modifier/client/{id}', 'edit')->name('modifierClient')->middleware(EnsureUserAdmin::class);
+        Route::post('/update', 'update')->name('updateClient')->middleware(EnsureUserAdmin::class);
+        Route::get('/ajouter/client', 'create')->name('ajouterClient');
+        Route::get('/client/{id}', 'show')->name('detailClient');
+        Route::post('/enregistrerClient', 'store')->name('enregistrementClient');
+        Route::post('/supprimerClient', 'destroy')->name('supprimerUnClient')->middleware(EnsureUserAdmin::class);
+        Route::post('/filtrerClient', 'index')->name('filtrerClients');
+    });
+
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/index', 'index')->name('indexUser');
+        Route::get('/employes', 'index')->name('employes')->middleware(EnsureUserAdmin::class);
+        Route::get('/newEmploye', 'create')->name('newEmploye')->middleware(EnsureUserAdmin::class);
+        Route::post('/confirmAddEmploye', 'store')->name('insertEmploye')->middleware(EnsureUserAdmin::class);
+        Route::get('/employe/{id}', 'show')->name('employe')->middleware(EnsureUserAdmin::class);
+        Route::get('/modificationEmploye/{id}', 'edit')->name('modificationEmploye')->middleware(EnsureUserAdmin::class);
+        Route::post('/modificationEmploye/{id}', 'update')->name('enregistrementEmploye')->middleware(EnsureUserAdmin::class);
+        Route::post('/supprimerEmploye', 'destroy')->name('supprimerUnEmploye')->middleware(EnsureUserAdmin::class);
+        Route::post('/filtrerEmp', 'index')->name('filtrerEmployer')->middleware(EnsureUserAdmin::class);
+    });
 });
 
-Route::controller(TransactionController::class)->group(function () {
-    Route::get('/gestionCommandes', 'index')->name('gestionCommandes');
-    Route::get('/ajouterCommande', 'create')->name('ajouterCommande');
-    Route::get('/consulterCommande', 'index')->name('consulterCommande');
-    Route::get('/listerCommandes', 'index')->name('listerCommandes');
-    Route::get('/commande/{id}', 'show')->name('detailCommande');
-    Route::get('/resumeCommande', 'index')->name('resumeCommande');
-    Route::get('/extraitCommande/{id}', 'edit')->name('extraitCommande');
-    Route::get('/editerCommande/{id}', 'show')->name('editerCommande');
-    Route::get('/editerCommande/commande', 'update')->name('enregistrerCommande');
-    // Route::get('/supprimerCommande', 'destroy')->name('supprimerCommande');
-});
-
-
-Route::controller(ProduitController::class)->group(function () {
-    Route::get('/promotions', 'index')->name('promotions');
-    Route::get('/newPromotion', 'create')->name('newPromotion');
-    Route::get('/gestion/produits', 'store')->name('gestionProduits');
-    Route::get('/gestion/inventaire', 'index')->name('gestionInventaire');
-    Route::post('/gestion/inventaire/{nom}', 'update')->name('search');
-});
-
-Route::controller(ProfileController::class)->group(function () {
-    Route::get('/index', 'index')->name('indexUser');
-    Route::get('/employes', 'index')->name('employes');
-    Route::get('/newEmploye', 'create')->name('newEmploye');
-    Route::post('/confirmAddEmploye', 'store')->name('insertEmploye');
-    Route::get('/employe/{id}', 'show')->name('employe');
-    Route::get('/modificationEmploye/{id}', 'edit')->name('modificationEmploye');
-    Route::post('/modificationEmploye/{id}', 'update')->name('enregistrementEmploye');
-    Route::post('/supprimerEmploye', 'destroy')->name('supprimerUnEmploye');
-    Route::post('/filtrerEmp', 'index')->name('filtrerEmployer');
-});
-
-Route::controller(DelaiCeuilletteController::class)->group(function () {
-    Route::get('/delais', 'index')->name('delais');
-    Route::get('/modifier/delais', 'edit')->name('modifierDelais');
-    Route::get('/definir/delais', 'create')->name('definirDelais');
-});
-
-Route::controller(ClientController::class)->group(function () {
-    Route::get('/clients', 'index')->name('consulterClient');
-    Route::get('/modifier/client/{id}', 'edit')->name('modifierClient');
-    Route::post('/update', 'update')->name('updateClient');
-    Route::get('/ajouter/client', 'create')->name('ajouterClient');
-    Route::get('/client/{id}', 'show')->name('detailClient');
-    Route::post('/enregistrerClient', 'store')->name('enregistrementClient');
-    Route::post('/supprimerClient', 'destroy')->name('supprimerUnClient');
-    Route::post('/filtrerClient', 'index')->name('filtrerClients');
-});
 
 require __DIR__ . '/auth.php';
