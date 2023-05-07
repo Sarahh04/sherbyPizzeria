b56ed9e5b09201d9b7d11419b31ae4293657b082
ThirdPartyHugo <hpskate26@gmail.com>
2023-05-07 16:08:07 -0400

    s

--- public/js/script.js
+++ public/js/script.js
@@ -250,10 +250,22 @@ function stopForm(evt)
 
 
 
+var url = "{{ route('gestionProduits') }}";
 
+if(window.location.href === url){
+let switchBtn = document.querySelector('.container-button-modif>button');
 
+switchBtn.addEventListener('click', showForm)
 
+function showForm(e){
+let formMenu = document.querySelector('.form-gestion');
+let buttonMenu = document.querySelector('.container-button-modif')
 
+formMenu.classList.toggle('form-hidden');
+buttonMenu.classList.toggle('form-hidden');
+}
+
+}
 
 /*********************************************************/
 /* Charger les listeners                                 */
