f8ed22fbefd9449ff0e6d6941d9c18d63f61b94e
ThirdPartyHugo <hpskate26@gmail.com>
2023-05-07 15:53:09 -0400

    d

--- public/js/script.js
+++ public/js/script.js
@@ -253,18 +253,6 @@ function stopForm(evt)
 
 
 
-let switchBtn = document.querySelector('.container-button-modif>button');
-
-switchBtn.addEventListener('click', showForm)
-
-function showForm(e){
-let formMenu = document.querySelector('.form-gestion');
-let buttonMenu = document.querySelector('.container-button-modif')
-
-formMenu.classList.toggle('form-hidden');
-buttonMenu.classList.toggle('form-hidden');
-}
-
 
 
 /*********************************************************/
